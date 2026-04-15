98f1d7e414123a1ddeb61cd5fa7cc663669ccf02
이재호 <ljh4367@naver.com>
2026-04-15 10:28:33 +0900

    ✨ Feat[S14P31E102-115]: 위치 권한 처리 및 현재 위치 매니저 추가

--- FE/app/src/main/java/com/ssafy/e102/eumgil/app/AppContainer.kt
+++ FE/app/src/main/java/com/ssafy/e102/eumgil/app/AppContainer.kt
@@ -1,6 +1,10 @@
 package com.ssafy.e102.eumgil.app
 
 import android.content.Context
+import com.ssafy.e102.eumgil.core.location.AndroidCurrentLocationManager
+import com.ssafy.e102.eumgil.core.location.AndroidLocationPermissionManager
+import com.ssafy.e102.eumgil.core.location.CurrentLocationManager
+import com.ssafy.e102.eumgil.core.location.LocationPermissionManager
 import com.ssafy.e102.eumgil.data.local.datasource.InitSettingsLocalDataSource
 import com.ssafy.e102.eumgil.data.local.datastore.initSettingsDataStore
 import com.ssafy.e102.eumgil.data.repository.DefaultInitSettingsRepository
@@ -9,9 +13,17 @@ import com.ssafy.e102.eumgil.data.repository.InitSettingsRepository
 class AppContainer(
     context: Context,
 ) {
+    private val appContext = context.applicationContext
+
     private val initSettingsLocalDataSource =
-        InitSettingsLocalDataSource(dataStore = context.applicationContext.initSettingsDataStore)
+        InitSettingsLocalDataSource(dataStore = appContext.initSettingsDataStore)
 
     val initSettingsRepository: InitSettingsRepository =
         DefaultInitSettingsRepository(localDataSource = initSettingsLocalDataSource)
+
+    val locationPermissionManager: LocationPermissionManager =
+        AndroidLocationPermissionManager(context = appContext)
+
+    val currentLocationManager: CurrentLocationManager =
+        AndroidCurrentLocationManager(context = appContext)
 }
---
+++ FE/app/src/main/java/com/ssafy/e102/eumgil/core/location/CurrentLocationManager.kt
@@ -0,0 +1,13 @@
+package com.ssafy.e102.eumgil.core.location
+
+import kotlinx.coroutines.flow.StateFlow
+
+interface CurrentLocationManager {
+    val latestLocation: StateFlow<LocationSnapshot?>
+
+    fun refreshLatestLocation()
+
+    fun startLocationUpdates()
+
+    fun stopLocationUpdates()
+}

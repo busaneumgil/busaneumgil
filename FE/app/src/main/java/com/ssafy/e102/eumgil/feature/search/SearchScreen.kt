1e386313f8211f568a36338d13e4f8a87a41b96a
이재호 <ljh4367@naver.com>
2026-04-29 10:26:36 +0900

    🐛 fix[S14P31E102-320]: 전역 상단 인셋 중복 적용 제거

--- FE/app/src/main/java/com/ssafy/e102/eumgil/feature/search/SearchScreen.kt
+++ FE/app/src/main/java/com/ssafy/e102/eumgil/feature/search/SearchScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -102,6 +103,7 @@ private fun SearchTopBar(
             modifier =
                 Modifier
                     .fillMaxWidth()
+                    .statusBarsPadding()
                     .padding(horizontal = EumSpacing.small, vertical = EumSpacing.xxSmall),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(EumSpacing.small),

215716000c8716e6a41f3a9ffba81a099a55f765
이재호 <ljh4367@naver.com>
2026-04-29 11:07:43 +0900

    🐛 fix[S14P31E102-320]: MAP-01 하단 탭 레이아웃과 아이콘 리소스 정합성 수정

--- FE/app/src/main/java/com/ssafy/e102/eumgil/app/navigation/TopLevelDestination.kt
+++ FE/app/src/main/java/com/ssafy/e102/eumgil/app/navigation/TopLevelDestination.kt
@@ -8,29 +8,34 @@ sealed class TopLevelDestination(
     val route: AppRoute,
     @StringRes val labelRes: Int,
     @DrawableRes val iconRes: Int,
+    val iconSizeDp: Int,
 ) {
     data object Map : TopLevelDestination(
         route = TopLevelRoute.Map,
         labelRes = R.string.route_map,
         iconRes = R.drawable.ic_nav_home,
+        iconSizeDp = 30,
     )
 
     data object SavedRoute : TopLevelDestination(
         route = TopLevelRoute.SavedRoute,
         labelRes = R.string.route_saved_route,
         iconRes = R.drawable.ic_nav_bookmark_outline,
+        iconSizeDp = 30,
     )
 
     data object Report : TopLevelDestination(
         route = ReportRoute.Report,
         labelRes = R.string.route_report,
         iconRes = R.drawable.ic_nav_report,
+        iconSizeDp = 30,
     )
 
     data object MyPage : TopLevelDestination(
         route = TopLevelRoute.MyPage,
         labelRes = R.string.route_my_page,
         iconRes = R.drawable.ic_nav_mypage,
+        iconSizeDp = 30,
     )
 
     companion object {

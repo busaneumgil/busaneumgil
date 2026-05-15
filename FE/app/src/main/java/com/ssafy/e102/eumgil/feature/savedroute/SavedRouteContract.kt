daea0f897c27e6592867e4b9ab6522af4891a184
김응서 <ksy_624@naver.com>
2026-05-15 15:52:50 +0900

    🐛 Fix[S14P31E102-421]: 북마크 카드와 편집 삭제 UX 정리

--- FE/app/src/main/java/com/ssafy/e102/eumgil/feature/savedroute/SavedRouteContract.kt
+++ FE/app/src/main/java/com/ssafy/e102/eumgil/feature/savedroute/SavedRouteContract.kt
@@ -70,6 +70,8 @@ sealed interface SavedRouteUiAction {
 
     data object EditDoneClicked : SavedRouteUiAction
 
+    data object DeleteSelectedClicked : SavedRouteUiAction
+
     data object ExploreMapClicked : SavedRouteUiAction
 
     data object RouteSettingClicked : SavedRouteUiAction

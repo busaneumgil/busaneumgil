3ead4060fbc0871de68c236dc44b3fb9f21856f1
유준호 <ryuwon.dev@gmail.com>
2026-05-18 00:24:33 +0900

    🐛 Fix[S14P31E102-225]: voice analyze 인증을 적용

--- BE/src/main/java/com/ssafy/e102/global/security/config/SecurityConfig.java
+++ BE/src/main/java/com/ssafy/e102/global/security/config/SecurityConfig.java
@@ -76,6 +76,8 @@ public class SecurityConfig {
 				.authenticated()
 				.requestMatchers("/places", "/places/**")
 				.authenticated()
+				.requestMatchers("/voice/**")
+				.authenticated()
 				.requestMatchers("/routes/**", "/route-ratings/**")
 				.authenticated()
 				.requestMatchers("/hazard-reports", "/hazard-reports/**")
